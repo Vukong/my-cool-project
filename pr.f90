fea8a756ae471f10df0dc3365de52d9163f7ee44
Ann <sedzin.li@mail.ru>
2020-04-18 04:13:03 +0300

    draw another rect

--- pr.f90
+++ pr.f90
@@ -6,6 +6,7 @@ call gsetbgcolor(win,'white'//char(0))
 call gclr(win)
 call newcolor(win,'red'//char(0))
 call drawrect(win,0.,10.,50.,50.)
+call drawrect(win,100.,100.,120.,120.)
 call ggetch(key)
 call gclose(win)
 end
